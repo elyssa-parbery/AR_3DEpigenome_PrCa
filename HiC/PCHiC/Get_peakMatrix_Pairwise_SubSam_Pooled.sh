c62e01108c0fe9ffd4aa05f8835f9d44b4dfca6d
Elyssa Campbell <57244865+elyssa-parbery@users.noreply.github.com>
2024-12-10 17:19:07 +1100

    Add files via upload

---
+++ HiC/PCHiC/Get_peakMatrix_Pairwise_SubSam_Pooled.sh
@@ -0,0 +1,17 @@
+
+CONFIGDIR=/scratch/tr07/PCHiC_Level_2_LNCaP_DHT_SubSam/config
+OUTDIR=/scratch/tr07/ec2963/PhD_Proj1_AR_Sig/PCHiC_Level_3/BothDHT_SubSam/PCHi-C_3kb_Res
+
+module load R/4.2.1 intel-compiler/2021.6.0
+
+#update names file to include samples from both time courses
+
+#Rscript makePeakMatrix.R \
+#your_full_path_to/names_file.txt \
+#hacat_myla_DS_peakMatrix
+for TIMEPOINT in 05h 2h 4h 16h 05mins 10mins 20mins 30mins
+do
+	mkdir ${OUTDIR}/Pairwise_${TIMEPOINT}_Pooled
+	Rscript ${CONFIGDIR}/makePeakMatrix_EC.R --twopass --notrans --rda --var cd --scorecol score ${CONFIGDIR}/names-file_3kb_${TIMEPOINT}_Pooled --cutoff 7 ${OUTDIR}//Pairwise_${TIMEPOINT}_Pooled/peakMatrix_LNCaP_${TIMEPOINT}_DHT_Pooled_SubSam_min7_3Kb.txt
+done
+
